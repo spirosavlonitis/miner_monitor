3e7dae7c4baa0c52b9185c91b13b41962a81ad2f
Spiros Avlonitis <spirosa84@hotmail.com>
2018-07-10 23:51:30 +0300

    trimming down code

--- main.c
+++ main.c
@@ -5,41 +5,44 @@
 #define MAXTEMP	 81
 #define MAXCHARS 1000
 #define MINUTE	 60
-#define DEAULT   "mine_classic"
+#define MON_INRERVAL	5
+
+#define COMMAND  "daemon mine_classic"
+#define PIDOF 	 "pidof ethminer"
 
 void recover(FILE *,char *);
 
 int main(int argc, char *argv[])
 {
 	int temp;
-	char path[MAXCHARS],command[MAXCHARS],*pidof;
+	char path[MAXCHARS];
 	FILE *lg;
 	struct passwd *pwd;
 
-	sprintf(command,"daemon %s",(argc < 2) ? DEAULT : *(argv+1));
-	pidof = (argc < 2) ? "pidof ethminer" : "pidof miner";
 	pwd = getpwuid(geteuid());
-	sprintf(path,"/home/%s/gpu_temp.md",pwd->pw_name);
+	snprintf(path, sizeof(path),"/home/%s/gpu_temp.md",pwd->pw_name);
 
-	while (1) {
+	for (; ;) {
 		lg = fopen(path,"a");
 		temp = logtemp(lg);
-		if (WEXITSTATUS(system(pidof)) == 1)
-			system(command);
+		if (WEXITSTATUS(system(PIDOF)) == 1)
+			system(COMMAND);
 		else if (temp >= MAXTEMP)
-			recover(lg,pidof);
+			recover(lg,PIDOF);
 		fclose(lg);
-		sleep(5*MINUTE);
+		sleep(MON_INRERVAL*MINUTE);
 	}
 	exit(0);
 }
+
+#define RCV_INTERVAL  10
 
 void recover(FILE *lg,char *pidof)
 {
 	char	kill[MAXCHARS];
 	long	timeout;
 
-	timeout = 10 * MINUTE;
+	timeout = RCV_INTERVAL * MINUTE;
 	fputs("Entered recovery mode\n",lg);
 	fputs("Entered recovery mode\n",stderr);
 	sprintf(kill,"kill -9 $(%s)",pidof);
