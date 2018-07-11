4136bcf2ed03c72843d3b65e4f122e922b56706a
Spiros Avlonitis <spirosa84@hotmail.com>
2018-07-12 02:13:01 +0300

    reset to daemon

--- main.c
+++ main.c
@@ -7,55 +7,57 @@
 #define MINUTE	 60
 #define MON_INRERVAL	5
 
-#define COMMAND  "mine_classic"
+#define COMMAND  "daemon mine_classic"
 #define PIDOF 	 "pidof ethminer"
 
 void recover(FILE *,char *);
 
-int main(int argc, char  *argv[])
+int l;
+
+int main(int argc, char *argv[])
 {
 	int temp;
 	char path[MAXCHARS];
-	pid_t	parent;
 	FILE *lg;
 	struct passwd *pwd;
 
 	pwd = getpwuid(geteuid());
 	snprintf(path, sizeof(path),"/home/%s/gpu_temp.md",pwd->pw_name);
 	l = 1;
-	parent = fork();
 
 	for (; ;) {
-		if (parent){	/* parent process handles temperature */
-			if (l)
-				lg = fopen(path,"a");
-			temp = logtemp((l) ? lg : stderr);
-			
-			if (temp >= MAXTEMP)
-				recover((l) ? lg : stderr, PIDOF);
-			if (l)
-				fclose(lg);
-		}else if (WEXITSTATUS(system(PIDOF)) == 1) /* forked process monitors connection status */
-				system(COMMAND);
-		sleep(MON_INRERVAL*MINUTE);		
+		lg = fopen(path,"a");
+		if (l)
+			temp = logtemp(lg);
+
+		if (WEXITSTATUS(system(PIDOF)) == 1)
+			system(COMMAND);
+		else if (temp >= MAXTEMP)
+			recover(lg,PIDOF);
+		
+		fclose(lg);
+		sleep(MON_INRERVAL*MINUTE);
 	}
 	exit(EXIT_SUCCESS);
 }
 
 #define RCV_INTERVAL  10
 
-void recover(FILE *fp,char *pidof)
+void recover(FILE *lg,char *pidof)
 {
 	char	kill[MAXCHARS];
 	long	timeout;
 
 	timeout = RCV_INTERVAL * MINUTE;
-	fputs("Entered recovery mode\n", fp);
+	fputs("Entered recovery mode\n", (l) ? lg : stderr);
 	snprintf(kill, sizeof(kill),"kill -9 $(%s)",pidof);
 	system(kill);
 	while (timeout > 0){
-		fprintf(fp,"Time until retry %ldmin\n",timeout/MINUTE);
-		fflush(fp);
+		if (l) {
+			fprintf(lg,"Time until retry %ldmin\n",timeout/MINUTE);
+			fflush(lg);
+		}else
+			fprintf(stderr,"Time until retry %ldmin\n",timeout/MINUTE);
 
 		timeout -= MINUTE;
 		sleep(MINUTE);
