32a8715a1110310b7861922bac453032d836114a
Spiros Avlonitis <spirosa84@hotmail.com>
2018-08-01 03:38:36 +0300

    trimed recovery function

--- recover.c
+++ recover.c
@@ -2,22 +2,20 @@
 
 #define RCV_INTERVAL  10
 
-void recover(FILE *lg,char *pidof)
+void recover(FILE *fp,char *pidof)
 {
 	char	kill[MAXCHARS];
 	long	timeout;
 
 	timeout = RCV_INTERVAL * MINUTE;
-	fputs("Entered recovery mode\n", (l) ? lg : stderr);
+	fputs("Entered recovery mode\n", fp);
 	snprintf(kill, sizeof(kill),"kill -9 $(%s)",pidof);
 	system(kill);
-	while (timeout > 0){
-		if (l) {
-			fprintf(lg,"Time until retry %ldmin\n",timeout/MINUTE);
-			fflush(lg);
-		}else
-			fprintf(stderr,"Time until retry %ldmin\n",timeout/MINUTE);
 
+	while (timeout > 0){
+		fprintf(stderr,"Time until retry %ldmin\n",timeout/MINUTE);
+		if (l) 
+			fflush(fp);
 		timeout -= MINUTE;
 		sleep(MINUTE);
 	}
